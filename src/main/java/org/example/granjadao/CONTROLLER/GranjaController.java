f6aebc5d61a4fbbed3de042f02b1d4a63c94793f
RunnerBunny483 <runnerbunny483@gmail.com>
2025-03-02 20:53:00 +0100

    Borrados imports que sobraban

--- src/main/java/org/example/granjadao/CONTROLLER/GranjaController.java
+++ src/main/java/org/example/granjadao/CONTROLLER/GranjaController.java
@@ -5,10 +5,6 @@ import org.example.granjadao.MODEL.Almacen;
 import org.example.granjadao.MODEL.Animal;
 import org.example.granjadao.MODEL.Producto;
 import org.example.granjadao.MODEL.Trabajador;
-import org.example.granjadao.REPOSITORY.AlmacenRepository;
-import org.example.granjadao.REPOSITORY.AnimalRepository;
-import org.example.granjadao.REPOSITORY.ProductoRepository;
-import org.example.granjadao.REPOSITORY.TrabajadorRepository;
 import org.example.granjadao.SERVICE.GranjaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
@@ -16,7 +12,6 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.bind.JAXBException;
 import java.io.IOException;
 import java.util.List;
 
