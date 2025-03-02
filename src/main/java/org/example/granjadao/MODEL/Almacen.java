af179589db11af1935e36fe33d81d3b2adb7b8f9
RunnerBunny483 <runnerbunny483@gmail.com>
2025-03-02 12:25:21 +0100

    Controller

--- src/main/java/org/example/granjadao/MODEL/Almacen.java
+++ src/main/java/org/example/granjadao/MODEL/Almacen.java
@@ -10,7 +10,7 @@ import javax.xml.bind.annotation.*;
 @AllArgsConstructor
 @XmlRootElement(name = "almacen")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class Establecimiento {
+public class Almacen {
     private Integer id;
     private String nombre;
     private String ubicacion;
