73771f3c0bf21093dff8ceed427acb6aa7b2c22e
RunnerBunny483 <runnerbunny483@gmail.com>
2025-03-02 12:22:09 +0100

    Controller

--- src/main/java/org/example/granjadao/SERVICE/GranjaService.java
+++ src/main/java/org/example/granjadao/SERVICE/GranjaService.java
@@ -1,4 +1,22 @@
 package org.example.granjadao.SERVICE;
 
+import org.example.granjadao.MODEL.Animal;
+import org.example.granjadao.REPOSITORY.AnimalRepository;
+import org.example.granjadao.REPOSITORY.ProductoRepository;
+import org.example.granjadao.REPOSITORY.TrabajadorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GranjaService {
+    @Autowired
+    private AnimalRepository animalRepository;
+
+    @Autowired
+    private TrabajadorRepository trabajadorRepository;
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+
 }
