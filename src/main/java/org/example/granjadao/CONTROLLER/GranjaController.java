cfb2659edd7bc078d546ccd8bba29cb4f48591f1
RunnerBunny483 <runnerbunny483@gmail.com>
2025-03-02 12:19:19 +0100

    Controller

--- src/main/java/org/example/granjadao/CONTROLLER/GranjaController.java
+++ src/main/java/org/example/granjadao/CONTROLLER/GranjaController.java
@@ -1,4 +1,13 @@
 package org.example.granjadao.CONTROLLER;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/granja")
+@CacheConfig(cacheNames = "granja")
 public class GranjaController {
+
+
 }
