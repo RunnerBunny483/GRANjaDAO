04d5ac0afc61caa3457acbfefdadbd04d1366bcc
RunnerBunny483 <runnerbunny483@gmail.com>
2025-03-02 12:40:40 +0100

    Controller

--- src/main/java/org/example/granjadao/MODEL/Almacen.java
+++ src/main/java/org/example/granjadao/MODEL/Almacen.java
@@ -14,4 +14,6 @@ public class Almacen {
     private Integer id;
     private String nombre;
     private String ubicacion;
+    private Integer numero_trabajadores;
+    private Double metrosCuadrados;
 }
