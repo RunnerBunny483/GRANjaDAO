1f8a5f7ae8e63b91d1eca3265d0731f7886ac4b8
xpanchamcode <marcospjob.5@gmail.com>
2025-03-02 13:47:00 +0100

    Push controller

--- src/main/java/org/example/granjadao/CONTROLLER/GranjaController.java
+++ src/main/java/org/example/granjadao/CONTROLLER/GranjaController.java
@@ -1,5 +1,6 @@
 package org.example.granjadao.CONTROLLER;
 
+import jakarta.validation.Valid;
 import org.example.granjadao.MODEL.Almacen;
 import org.example.granjadao.MODEL.Animal;
 import org.example.granjadao.MODEL.Producto;
@@ -11,10 +12,9 @@ import org.example.granjadao.REPOSITORY.TrabajadorRepository;
 import org.example.granjadao.SERVICE.GranjaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
@@ -26,29 +26,48 @@ import java.util.List;
 public class GranjaController {
 
     private GranjaService granjaService;
-    private AnimalRepository animalRepository;
-    private TrabajadorRepository trabajadorRepository;
     private ProductoRepository productoRepository;
 
     @Autowired
-    public GranjaController(GranjaService granjaService, AnimalRepository animalRepository, TrabajadorRepository trabajadorRepository, ProductoRepository productoRepository) {
+    public GranjaController(GranjaService granjaService, ProductoRepository productoRepository) {
         this.granjaService = granjaService;
-        this.animalRepository = animalRepository;
-        this.trabajadorRepository = trabajadorRepository;
         this.productoRepository = productoRepository;
     }
 
-    //GET
+    //----- GET -----
+
     @GetMapping("/trabajadores")
     public ResponseEntity<List<Trabajador>> getTrabajadores() {
-        List<Trabajador> listaTrabajadpres= trabajadorRepository.findAll();
+        List<Trabajador> listaTrabajadpres= granjaService.getAllTrabajadores();
         return ResponseEntity.ok(listaTrabajadpres);
+    }
+
+    @Cacheable
+    @GetMapping("/trabajadores/{id}")
+    public ResponseEntity<?> getTrabajadorById(@PathVariable String dni) {
+        try{
+            Trabajador trabajador = granjaService.findTrabajadorById(dni);
+            return ResponseEntity.ok().body(trabajador);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
     }
 
     @GetMapping("/animales")
     public ResponseEntity<List<Animal>> getAnimales() {
         List<Animal> listaAnimales= granjaService.getAllAnimales();
         return ResponseEntity.ok(listaAnimales);
+    }
+
+    @Cacheable
+    @GetMapping("/animales/{id}")
+    public ResponseEntity<?> getAnimalById(@PathVariable Integer id) {
+        try{
+            Animal animal = granjaService.findAnimalById(id);
+            return ResponseEntity.ok().body(animal);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
     }
 
     @GetMapping("/productos")
@@ -62,7 +81,28 @@ public class GranjaController {
         List<Almacen> listaAlamacenes= granjaService.getAllAlmacenes();
         return ResponseEntity.ok(listaAlamacenes);
     }
-    //POST
-    //PUT
-    //DELETE
+
+    //----- POST -----
+
+    @PostMapping("/animales")
+    public ResponseEntity<?> addAnimal(@Valid @RequestBody Animal animal) {
+        try{
+            Animal animalSave = granjaService.saveAnimal(animal);
+            return ResponseEntity.status(201).body(animalSave);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/trabajadores")
+    public ResponseEntity<?> addTrabajador(@Valid @RequestBody Trabajador trabajador) {
+        try{
+            Trabajador trabajadorSave = granjaService.saveTrabajador(trabajador);
+            return ResponseEntity.status(201).body(trabajadorSave);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+    //----- PUT -----
+    //----- DELETE -----
 }
